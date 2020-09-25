970d0380c59396f7f40da0530013b07d65450439
George Tang <georgecctang@gmail.com>
2020-09-25 20:21:29 +0000

    first commit; copied solution

--- index.js
+++ index.js
@@ -1,29 +1,58 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+  constructor(username) {
+    this.username = username;
+    // Initialize balance with $0
+    this._balance = 0;
+    // create empty transactions log
+    this.transactions = [];
   }
 
-  commit() {
-    balance -= this.amount;
+  get balance() {
+    return this._balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
   }
 
 }
 
+class Transaction {
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+
+  commit() {
+    this.account._balance += this.value;
+    this.time = new Date();
+    this.account.addTransaction(this);
+  }
+}
+
+class Deposit extends Transaction {
+
+  // Update the balance in the account
+  get value() {
+    return this.amount;
+  }
+}
 
 
+class Withdrawal extends Transaction {
 
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
+  get value() {
+    return -this.amount;
+  }
+}
 
-t1 = new Withdrawal(50.25);
+
+const myAccount = new Account("snow-patrol");
+
+const t1 = new Deposit(1000, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(400, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
-
-console.log('Balance:', balance);
+console.log(myAccount);
